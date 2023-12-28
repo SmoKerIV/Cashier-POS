8fec91a44e7e5bba4b56fc7303b5d1145ead63ce
Baker Alazzawi <Baker.alazzawi0@gmail.com>
2023-12-28 13:54:43 +0300

    added products,catagory and invoice

---
+++ src/app/layout.js
@@ -0,0 +1,18 @@
+"use client";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { NextUIProvider } from "@nextui-org/react";
+import Header from "@/components/Header/header";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Header/>
+        <NextUIProvider>{children}</NextUIProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/components/Header/header.jsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import AppContainer from "../Container/container";
+import styles from "./header.module.css";
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <AppContainer width={1300}>
+        <div className={styles.content}>
+          <h1>Logo</h1>
+          <ul>
+            <li>
+              <Link href={"http://localhost:3000/"}>Home</Link>
+            </li>
+
+            <li>
+              <Link href={"/products"}>Products</Link>
+            </li>
+
+            <li>
+              <Link href={"/category"}>Categories</Link>
+            </li>
+
+            <li>
+              <Link href={"/invoices"}>Invoices</Link>
+            </li>
+          </ul>
+        </div>
+      </AppContainer>
+    </div>
+  );
+};
+
+export default Header;
